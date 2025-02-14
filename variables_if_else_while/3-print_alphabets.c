a53ea67fa961f0e7da013ec0a5e6180ecd55e046
root <10485@holbertonstudents.com>
2025-02-14 08:25:35 +0000

    for

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
-	int alphabet;
-	int ALPHABET;
+	int alphabet, ALPHABET;
 
 	for (alphabet = 97; alphabet <= 122; alphabet++)
 		putchar(alphabet);
 
 	for (ALPHABET = 65; ALPHABET < 91; ALPHABET++)
-		putchar(ALPHABET);	
+		putchar(ALPHABET);
 	putchar('\n');
 	return (0);
 }
