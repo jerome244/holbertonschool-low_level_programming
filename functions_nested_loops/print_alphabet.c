82b294bd3adcc659034d9320c0b5e3ea1324a1ab
root <10485@holbertstudents.com>
2025-02-14 21:29:27 +0000

    second compilation

--- functions_nested_loops/print_alphabet.c
+++ functions_nested_loops/print_alphabet.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include <unistd.h>
 #include "_putchar.c"
+
 /**
  * print_alphabet - display alphabet in lowercase
  * void
  *
- * Return: void
+ * Return: void.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 void print_alphabet(void)
 {
-	char c;
+        char c;
 	char d = '\n';
 
 	for (c = 'a'; c <= 'z'; c++)
