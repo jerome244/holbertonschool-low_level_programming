833b5f925bb8d42aa659df0089adc46e74a975f9
root <10485@holbertonstudents.com>
2025-02-16 07:55:34 +0000

    print hexadecimal char in lowercase

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,12 +6,15 @@
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i >= 15; i++)
-		putchar(i);
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 97; j < 103; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
