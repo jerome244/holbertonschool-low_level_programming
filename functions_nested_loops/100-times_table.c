f4367e6fd458190efdaff8c0b6a2772f50fdb473
root <10485@holbertonstudents.com>
2025-02-16 07:20:18 +0000

    multiplication tables

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,43 +9,37 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n > 0 || n < 15)
+	if (n < 0 || n > 15)
+		;
+	else
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			_putchar('0');
+			for (j = 1; j < n; j++)
 			{
 				k = i * j;
+				_putchar(',');
+				_putchar(' ');
+
 				if (k < 10)
 				{
-					if (j > 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + '0');
 				}
-				else
+				else if (k < 100)
 				{
-					if (k < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(k / 10 + '0');
-						_putchar(k % 10 + '0');
-					}
-					else
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(k / 10 + '0');
-							_putchar(k % 10 + '0');
-							_putchar(k / 100 + '0');
-						}
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
 				}
+				else
+				{	_putchar(k / 100 + '0');
+					_putchar(k % 10 / 10 + '0');
+					_putchar(k % 10 + '0');					
+				}
+
 			}
 			_putchar('\n');
 		}
