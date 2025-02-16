16b1e99869dac37333e455f83aa3676134b81b2e
root <10485@holbertonstudents.com>
2025-02-16 06:02:55 +0000

    if-for loops-printf

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print number entry to 98
+ * @n: int
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+		for ( ; n < 98 ; n++)
+			printf("%d, ", n);
+	else
+		for ( ; n > 98 ; n--)
+			printf("%d, ", n);
+	printf("%d \n", n);
+}
