3a7e46768773388aaf8fe59364160b89b4b63d56
root <10485@holbertonstudents.com>
2025-02-16 17:31:53 +0000

    multiples of 3 and 5

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			printf("%d", i);
+			if (i < 1023)
+				printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
