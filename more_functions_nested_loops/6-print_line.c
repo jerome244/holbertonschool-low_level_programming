5d77bc9a009b32f32efc823e844c4e0ab9b766a6
root <10485@holbertonstudents.com>
2025-02-24 09:50:50 +0000

    print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+		if (n > 0)
+			_putchar('_');
+		else
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
