58f2520f2054bde86f0cbdef08d8ff7b909509b4
root <10485@holbertonstudents.com>
2025-02-24 11:14:44 +0000

    print diagonales

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,18 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0 || n < 0)
 	{
-		for (j = 0; j != i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j != i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
-		_putchar('\n');
 	}
+	else if (n == 0)
+		_putchar('\n');
 }
