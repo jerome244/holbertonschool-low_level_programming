984060aa91e74847cf5e5592c156c2ee3da3843d
root <10485@holbertonstudents.com>
2025-02-24 14:22:19 +0000

    print 0 14 10times 3putchars

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,6 +7,23 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
-	
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				k = j % 10;
+			}
+			else
+			{
+				k = j;
+			}
+			_putchar(k + '0');
+		}
+		_putchar('\n');
+	}
+}
