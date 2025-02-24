cb43bbb2de512aac744d91ee050b93befca94262
root <10485@holbertonstudents.com>
2025-02-24 15:29:22 +0000

    reset pointer to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set an int pointer to 98
+ * @n: pointer int type
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c : reset pointer to 98;
