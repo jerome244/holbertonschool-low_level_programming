edfde579935809a562bec251f49bce31c8ed4482
root <10485@holbertonstudents.com>
2025-02-25 19:06:45 +0000

    print half of string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,22 +9,18 @@
 void puts_half(char *str)
 {
 	int i, j;
-	
+
 	for (i = 0; str[i]; i++)
 		;
 	if (i % 2 == 0)
 	{
 		for (j = i / 2; str[j]; j++)
-		{
 			_putchar(str[j]);
-		}
 	}
 	else
 	{
 		for (j = (i + 1) / 2; str[j]; j++)
-		{
 			_putchar(str[j]);
-		}
 	}
 	_putchar('\n');
 }
