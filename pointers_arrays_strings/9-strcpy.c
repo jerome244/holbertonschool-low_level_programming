76d949b099d37adf86d79ef0d16ac5d8f81b30e4
root <10485@holbertonstudents.com>
2025-02-25 21:14:08 +0000

    copy a string to a character array

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - pointer on function
+ * copy a string into a char array dest
+ * @dest: char array for dest
+ * @src: char array of source
+ * Return: char pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i] + '\0';
+	}
+	return (dest);
+}
