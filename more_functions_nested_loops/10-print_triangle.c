6f224f07f73e275fa413c5e7d0ec9b17c3b6bd96
root <10485@holbertonstudents.com>
2025-02-25 15:08:19 +0000

    print triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,13 +7,18 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
-	for (i = 0; i < size; i++) 
-	{
-		for (j = 0; j < (size - i - 1); j++)
-			_putchar(' ');
-		for (k = 0; k <= i; k++)
-			_putchar(35);
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		int i, j, k;
+		for (i = 0; i < size; i++) 
+		{
+			for (j = 0; j < (size - i - 1); j++)
+				_putchar(' ');
+			for (k = 0; k <= i; k++)
+				_putchar(35);
+			_putchar('\n');
+		}
 	}
 }
