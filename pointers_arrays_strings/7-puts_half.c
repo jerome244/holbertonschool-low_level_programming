01adce0505fc4d295828940dccaab6527c5951a9
root <10485@holbertonstudents.com>
2025-02-25 18:21:01 +0000

    print half of string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of strings
+ * if odd arround int
+ * @str: character array
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; str[i]; i++)
+		;
+	j = i % 2 != 0;
+	k = j ? j + 1 : i;
+	for (k = k / 2; str[k]; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
