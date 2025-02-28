f5cd2140f4a2fb0e59b4e95e73a4df1f82118f17
root <10485@holbertonstudents.com>
2025-02-28 10:21:48 +0000

    generate random password

---
+++ more_functions_nested_loops/101-keygen.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate a random password
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int N = 10;
+	int randomizer = 0;
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[N];
+
+	srand((unsigned int)(time(NULL)));
+	randomizer = rand() % 4;
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+	printf("\n");
+	return (0);
+}
