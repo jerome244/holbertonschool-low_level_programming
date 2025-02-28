4218d4d49366dee18821ac08cfb38c206d73a4b9
root <10485@holbertonstudents.com>
2025-02-28 07:47:00 +0000

    print numbers factor

---
+++ functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print prime numbers
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int c = 2;
+
+	while (n > 1)
+	{
+		if (n % c == 0)
+		{
+			printf("%ld ", c);
+			n /= c;
+		}
+		else
+			c++;
+	}
+	printf("\n");
+	return (0);
+}
