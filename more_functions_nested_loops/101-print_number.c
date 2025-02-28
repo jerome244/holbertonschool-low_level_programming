978f201de707bab37f6a2cd8f4f9fc64014ba8c5
root <10485@holbertonstudents.com>
2025-02-28 09:28:32 +0000

    print number

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,19 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n != 0)
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		else if (n / 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
 	}
-	else if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else
+	{
+		_putchar('0');
+	}
 }
