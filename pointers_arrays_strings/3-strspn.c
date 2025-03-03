bb2c64cc04e126f293d555fe78b401c3e1218c87
root <10485@holbertonstudents.com>
2025-03-03 12:18:59 +0000

    count number of characters in first string before meet occurences of second string

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+/**
+ * _strspn - count number
+ * @s: string 1
+ * @accept: string 2
+ * Return: number of characters before occurence is met
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0;
+
+	while (s[i] != accept[j] && s[i] != '\0' && accept[j] != '\0')
+	{
+		if (s[i] == accept[j])
+		{
+			return (i + 1);
+		}
+		i++;
+	}
+	return (i + 1);
+}
