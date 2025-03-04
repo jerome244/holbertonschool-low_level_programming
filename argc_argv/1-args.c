fc7b0295735067fa25d6ec0db40e5dfbcb07322f
root <10485@holbertonstudents.com>
2025-03-04 16:29:56 +0000

    print number of arguments passed into the program

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,7 +11,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[argc]; ++i, argc++)
+	for (i = 0; argv[argc]; i++)
+		;
+	for (i = 0; i < argc; ++i)
 		;
 	printf("%d\n", i);
 	return (0);
