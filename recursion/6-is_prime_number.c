439a5a34f03e7e08486d2c48d474c52f2e172b14
root <10485@holbertonstudents.com>
2025-03-04 10:00:23 +0000

    check if a number is a prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+/**
+ * is_prime_number - indicate if entry is a prime number
+ * 
+ * @n: entry
+ * Return: 1 if it is
+ * 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 2)
+		return ((n == 2) ? 1 : 0);
+	if (n % 2 == 0)
+		return (0);
+	if (2 * 2 > n)
+		return (1);
+	return (is_prime_number(2 + 1));
+}
