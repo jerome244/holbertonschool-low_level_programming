83b299360b1b94de6db1ebcce8273a92f1c5311e
root <10485@holbertonstudents.com>
2025-03-04 08:30:05 +0000

    return calculation of x raised by power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _pow_recusion - return value of x
+ * raised by power of y
+ *
+ * @x: receiver
+ * @y: powered of
+ * Return: if lower 0 return -1
+ * else return the result of calculation
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
