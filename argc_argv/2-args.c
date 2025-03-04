436b51b53f06ced1d3d5a2842cb214d5a13d97cf
root <10485@holbertonstudents.com>
2025-03-04 17:27:28 +0000

    print all arguments of main()

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 if no error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
