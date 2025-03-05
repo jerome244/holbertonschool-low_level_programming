451d6f61d3e8b27aa9ab03e2df162223a92f1879
root <10485@holbertonstudents.com>
2025-03-05 07:01:34 +0000

    multiply 2 int from command-line

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -47,15 +47,15 @@ int main(int argc, char *argv[])
 {
 	int i, j;
 
-	i = _atoi(argv[1]);
-	j = _atoi(argv[2]);
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (-1);
+		return (1);
 	}
 	else
 	{
+		i = _atoi(argv[1]);
+		j = _atoi(argv[2]);
 		printf("%d\n", i * j);
 		return (0);
 	}
