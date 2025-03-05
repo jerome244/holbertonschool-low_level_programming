8d453641bcb320955b3d8c4b53c8771dfae9fe17
root <10485@holbertonstudents.com>
2025-03-05 14:22:37 +0000

    add numbers from command line arguments

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -48,29 +48,20 @@ int _atoi(char *s)
 
 int main(int argc, char *argv[])
 {
-	int sum, i, j;
+	int i, j, sum = 0;
 
-	sum = 0;
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			for (j = 0; argv[i][j]; j++)
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 			{
-				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-				sum += _atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+		sum += _atoi(argv[i]);
 	}
-	else if (argc == 1)
-	{
-		printf("0\n");
-		return (1);
-	}
+	printf("%d\n", sum);
 	return (0);
 }
