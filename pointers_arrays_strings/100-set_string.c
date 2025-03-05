81403bc39121cd9a1e28b2048f98e2eba6fdfab9
root <10485@holbertonstudents.com>
2025-03-05 19:53:25 +0000

    modify strings pointed via another pointed string

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -3,50 +3,12 @@
 #include <stdlib.h>
 
 /**
- * _strlen - return length of string
- * @s: array of char
- * Return: int
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-		;
-
-	return (i);
-}
-/**
- * _strcpy - pointer on function
- * copy a string into a char array dest
- * @dest: char array for dest
- * @src: char array of source
- * Return: char pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	char *temp = dest;
-	int i;
-
-	for (i = 0; src[i]; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-	return (temp);
-}
-/**
- * set_string - copy content of str1 into str2
- * @s: pointed string source
- * @to: pointed string destination
+ * set_string - copy string source to string dest
+ * @s: source
+ * @to: dest
  */
 
 void set_string(char **s, char *to)
 {
-	int d = _strlen(*s);
-
-	to = malloc(d * sizeof(char));
-	to = _strcpy(to, *s);
+	*s = to;
 }
