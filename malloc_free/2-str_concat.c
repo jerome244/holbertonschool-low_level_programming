3e6022f7c0442118c0711c5e30936b9c7b28c454
root <10485@holbertonstudents.com>
2025-03-10 11:40:31 +0000

    concatenate 2 strings with malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -21,34 +21,32 @@ int _strlen(char *s)
 /**
  * str_concat - concatenate 2 strings
  * malloc style
- * @str1: first string
- * @str2: second string
+ * @s1: first string
+ * @s2: second string
  * Return: pointer of string concatenated with malloc
  */
 
-char *str_concat(char *str1, char *str2)
+char *str_concat(char *s1, char *s2)
 {
-	int s1, s2, s3, i, j;
+	int len1, len2, i;
 	char *ptr;
-	char *mt = "";
 
-	if (str1 == NULL)
-		str1 = mt;
-	if (str1 == NULL)
-		str2 = mt;
-	s1 = _strlen(str1);
-	s2 = _strlen(str2);
-	s3 = s1 + s2 + 1;
-	ptr = malloc(s3);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	ptr = malloc(len1 + len2 + 1);
 	if (ptr == NULL)
 		return (ptr);
-	for (i = 0; i < s1; i++)
+	for (i = 0; i < len1; i++)
 	{
-		ptr[i] = str1[i];
+		ptr[i] = s1[i];
 	}
-	for (j = 0; j < s1 + s2; i++, j++)
+	for ( ; i < len1 + len2; i++)
 	{
-		ptr[i] = str2[j];
+		ptr[i] = s2[i - len1];
 	}
 	ptr[i] = '\0';
 	return (ptr);
