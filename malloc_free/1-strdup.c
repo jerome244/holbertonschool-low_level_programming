9c79c520a812a894996efe30192515511817b988
root <10485@holbertonstudents.com>
2025-03-10 08:06:58 +0000

    duplicate string into allocated pointer string

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 /**
+ * _strlen - return length of string
+ * @s: array of char
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+        int i;
+
+        for (i = 0; s[i]; i++)
+                ;
+
+        return (i);
+}
+
+/**
  * _strdup - duplicate a string in allocated memory
  * @str: input string
  * Return: pointer to duplicated string
@@ -14,7 +30,7 @@ char *_strdup(char *str)
 	char *dup_offset;
 
 	/* Allocate memory for duplicate */
-	size = strlen(str);
+	size = _strlen(str);
 	ptr = ((char *)malloc(sizeof(char) * size + 1));
 	if (ptr == NULL)
 		return ((char *) NULL);
