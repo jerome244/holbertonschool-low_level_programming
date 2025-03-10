b7b19eaa9f32caf870c2d672805d3790ac5424e2
root <10485@holbertonstudents.com>
2025-03-10 18:32:49 +0000

    deallocate 2D array

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - deallocate 2D array
+ * @grid: 2D array
+ * @height: height of the array
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
