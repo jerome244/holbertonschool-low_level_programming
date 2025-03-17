f8fc3bc2e3e766a68dbf0b841fe3dcf0ac6425c4
root <10485@holbertonstudents.com>
2025-03-17 08:17:27 +0000

    print strings separated by a chosen separator string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings separated by a chosen separator string
+ * ended by a newline
+ * @separator: string to print between numbers
+ * @n: number of integers
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+	va_list parameters;
+
+	va_start(parameters, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(parameters, char *);
+		if (!string)
+			string = "(nil)";
+		printf("%s", string);
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(parameters);
+}
