24d0ed7c53d23903f29d21c43417e6f8ce77a342
root <10485@holbertonstudents.com>
2025-03-17 02:53:40 +0000

    execute existed functions on each element of an int array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,9 +1,12 @@
+#include <stddef.h>
+
 /**
  * array_iterator - execute an existing function on each element of an array
  * @array: array to iterate
  * @size: size of the array
  * @action: print_elem or print_elem_hex function
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
