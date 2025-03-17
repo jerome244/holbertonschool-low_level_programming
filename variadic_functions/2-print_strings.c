82a1ebb25c63f05f210087d5069cdbf78290399e
root <10485@holbertonstudents.com>
2025-03-17 08:19:07 +0000

    print strings separated by a chosen separator string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(parameters, n);
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(parameters, char *);
+		string = va_arg(parameters, char *);
 		if (!string)
 			string = "(nil)";
 		printf("%s", string);
