a00be02306effc0b75a6e65b07e3ff386e538f7c
root <10485@holbertonstudents.com>
2025-03-31 09:18:56 +0000

    fix: coding task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,13 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t length;
+	size_t length = 0;
 
 	while (h)
 	{
-		printf("[%i] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
 		length++;
+		h = h->next;
 	}
 	return (length);
 }
