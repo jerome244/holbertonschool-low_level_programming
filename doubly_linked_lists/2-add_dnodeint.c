93156d6c2cf80023a2edaff68d6b09e02f8a8166
root <10485@holbertonstudents.com>
2025-03-31 15:02:11 +0000

    feat: task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - prepends a node to a linked list
+ * @head: pointer to the head of a linked list
+ * @n: integer the new node should contain
+ * Return: pointer to the new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if(head)
+	{
+		new->next = *head;
+		if (*head)
+			*head = new;
+	}
+	else
+		new->next = NULL;
+	free(new);
+	return (new);
+}
