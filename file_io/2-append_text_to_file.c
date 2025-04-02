600aa085e63962b5237775806f4ec9e5fe09d937
root <10485@holbertonstudents.com>
2025-04-02 10:11:33 +0000

    fix: return statement

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+
+/**
  * append_text_to_file - appends text to a file
  * @filename: string name of file to append text to
  * @text_content: string to append
@@ -10,22 +29,17 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t fd, written = 0;
-	size_t len;
+	size_t len = _strlen(text_content);
 
 	if (filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
+	if (len)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
 		written = write(fd, text_content, len);
 	}
 	close(fd);
-	if (written == len)
-		return (1);
-	else
-		return (-1);
+	return (written == len ? 1 : -1);
 }
