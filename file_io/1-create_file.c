466cd4050f35fd32e6e1240df2c843c4502abf6f
root <10485@holbertonstudents.com>
2025-04-02 09:17:18 +0000

    fix: betty verification

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,8 +19,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (len = 0; text_content[len]; len++);
-		written = write(fd, text_content, len);
+		for (len = 0; text_content[len]; len++)
+			written = write(fd, text_content, len);
 	}
 	close(fd);
 	if (written == -1)
